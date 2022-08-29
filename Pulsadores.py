78f07ebe73e34d21e937103e2a055dbc60f4f6fa
Esteban <estebancoloradogonzalez@gmail.com>
2022-08-29 16:45:58 -0500

    Se completa los pulsadores

--- Pulsadores.py
+++ Pulsadores.py
@@ -14,6 +14,10 @@ gpio.setup(13, gpio.OUT)
 gpio.setup(15, gpio.OUT)
 gpio.setup(37, gpio.OUT)
 
+gpio.setup(35, gpio.IN, gpio.PUD_DOWN)
+gpio.setup(33, gpio.IN, gpio.PUD_DOWN)
+gpio.setup(31, gpio.IN, gpio.PUD_DOWN)
+
 def secuenciaUno():
 	contador = 0
 	while(contador != 2):
@@ -25,23 +29,23 @@ def secuenciaUno():
 		gpio.output(13, True)
 		gpio.output(15, True)
 		gpio.output(37, True)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(16, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(18, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(22, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(36, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(11, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(13, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(15, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(37, False)
-		time.sleep(2)
+		time.sleep(0.5)
 		contador = contador + 1
 
 def secuenciaDos():
@@ -55,19 +59,19 @@ def secuenciaDos():
 		gpio.output(13, True)
 		gpio.output(15, True)
 		gpio.output(37, True)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(36, False)
 		gpio.output(11, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(22, False)
 		gpio.output(13, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(18, False)
 		gpio.output(15, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(16, False)
 		gpio.output(37, False)
-		time.sleep(2)
+		time.sleep(0.5)
 		contador = contador + 1
 
 def secuenciaTres():
@@ -81,19 +85,19 @@ def secuenciaTres():
 		gpio.output(13, True)
 		gpio.output(15, True)
 		gpio.output(37, True)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(16, False)
 		gpio.output(37, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(18, False)
 		gpio.output(15, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(22, False)
 		gpio.output(13, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(36, False)
 		gpio.output(11, False)
-		time.sleep(2)
+		time.sleep(0.5)
 		contador = contador + 1
 
 def secuenciaCuatro():
@@ -107,19 +111,25 @@ def secuenciaCuatro():
 		gpio.output(13, True)
 		gpio.output(15, True)
 		gpio.output(37, True)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(18, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(36, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(13, False)
-		time.sleep(1)
+		time.sleep(0.5)
 		gpio.output(37, False)
-		time.sleep(2)
+		time.sleep(0.5)
 		contador = contador + 1
 
 def menu():
+	print("Presione una opcion")
 	while(True):
-		
+		if(gpio.input(35) == True):
+			secuenciaUno()
+		if(gpio.input(33) == True):
+			secuenciaDos()
+		if(gpio.input(31) == True):
+			secuenciaTres()
 
 menu()
